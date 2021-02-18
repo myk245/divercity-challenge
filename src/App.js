9d045a55116ab74fbc466aae582e6650869ff693
myk245 <myk245@nyu.edu>
2021-02-17 21:47:40 -0500

    set up app and initial fetch to api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from 'react'; 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    jobs: []
+  }
+
+  componentDidMount = () => {
+    fetch('https://divercity-test.herokuapp.com/jobs')
+      .then(resp => resp.json())
+      .then(data => this.setState({
+        jobs: data
+      }))
+    .then(() => console.log(this.state.jobs))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Divercity Frontend Challenge</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
