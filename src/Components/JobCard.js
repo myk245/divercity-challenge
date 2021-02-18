44461416704fbf040aaf87eaf2862c8f4e30ee5a
myk245 <myk245@nyu.edu>
2021-02-18 01:28:18 -0500

    add location

--- src/Components/JobCard.js
+++ src/Components/JobCard.js
@@ -4,11 +4,12 @@ const JobCard = (props) => {
    return (
       <div className="job-card">
          <h3>{props.job.title} at {props.job.company}</h3>
+         <p>{props.job.location}</p>
          <p>{props.job.description}</p>
          <p>Job Type: {props.job.job_type}</p>
-         <p>Number of Applicants: {props.job.applicant_count}</p>
          <p>Skills</p>
          {props.job.skills_tag.map(skill => <p>{skill}</p>)}
+         <p>Current Number of Applicants: {props.job.applicant_count}</p>
       </div>
    );
 }
