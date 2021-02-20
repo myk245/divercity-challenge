30658c2a0ec9a8a7b277e13af213224ddc2705f3
myk245 <myk245@nyu.edu>
2021-02-20 16:28:38 -0500

    clean up metadata

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,7 +4,11 @@ class Home extends React.Component {
    render() {
       return (
          <div>
-            <h1>Home</h1>
+            <h1 className="header">Welcome to Divercity</h1>
+            <p>Connecting underrepresented
+            professionals with
+            diverse-minded companies</p>
+            <p>Are you an underrepresented professional in search of your next opportunity? We’ve got you covered. Make new connections and get instant access to jobs with companies that value diversity.</p>
          </div>
       )
    }
