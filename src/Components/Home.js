ee6d38a18674b53bcf0ac92dadfa73901bfa4a9a
myk245 <myk245@nyu.edu>
2021-02-20 21:42:53 -0500

    home page done

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,17 +5,22 @@ class Home extends React.Component {
       return (
          <div>
             <h1 className="header">Welcome to Divercity</h1>
-            <div className="flex-wrapper">
-               <div className="row">
+               <div className="flex-container">
                   <div className="image-container">
                      <img className="image" alt="Divercity Home" src="Divercity-Home.png"></img>
                   </div>
                   <div className="text-container">
-                     <p className="home-text">Connecting underrepresented
-                     professionals with diverse-minded companies</p>
+                     <div className="column">
+                        <p className="column-item home-text">Connecting underrepresented
+                        professionals with diverse-minded companies</p>
+                        <div className="column-item secondary-text">
+                           <p className="text">Are you an underrepresented professional in search of your next opportunity? We’ve got you covered. Make new connections and get instant access to jobs with companies that value diversity.</p>
+                           <br></br>
+                           <a href="/jobs" className="button">Browse Jobs at Divercity</a>
+                        </div>
+                     </div>
                   </div>
                </div>
-            </div>
          </div>
       )
    }
--- src/Components/placeholder.js
+++
@@ -1,5 +0,0 @@
-
-
-<div className="container row">
-   <p>Are you an underrepresented professional in search of your next opportunity? We’ve got you covered. Make new connections and get instant access to jobs with companies that value diversity.</p>
-</div> 
